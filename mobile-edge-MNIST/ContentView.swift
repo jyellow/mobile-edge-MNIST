c008d1f5b630b0c8e3b80a3f684f7ad7cb688967
jyellow <johannyellow.cn@gmail.com>
2024-07-05 17:38:07 +0800

    Initial Commit

---
+++ mobile-edge-MNIST/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mobile-edge-MNIST
+//
+//  Created by Johann Yellow on 5/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mobile-edge-MNIST/mobile_edge_MNISTApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobile_edge_MNISTApp.swift
+//  mobile-edge-MNIST
+//
+//  Created by Johann Yellow on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct mobile_edge_MNISTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
